5111350b0736ae471a518effa879194c6a680def
JCesar <jcesarfb7@gmail.com>
2020-05-12 23:30:37 -0600

    creacion de proyecto con laravel

--- site/controllers/conexionBD.php
+++
@@ -1,9 +0,0 @@
-<?php
-//esto es para conectar la base de datos en la pagina web
-    $usuariobd="root";
-    $host="localhost";
-    $clave="root";
-    $basededatos="bdaluminiosyobras";
-    $conexion=mysqli_connect($host,$usuariobd,$clave,$basededatos)
-	or die("No se pudo conectar al servidor.");
-?>

905032d47a6d9aaa140a207fb72b01dcc6d969e0
JCesar <jcesarfb7@gmail.com>
2020-05-13 13:38:01 -0600

    a bueno

---
+++ AluminiosYObras/database/seeds/bienvenida.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class bienvenida extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+         DB::table('bienvenida')-> 
+         insert(array('mensajewelcome'=>'bienvenido a Aluminios y Obras',
+        	'subtitulo'=>'De primera calidad',
+        	'descripcion'=>'Somos la empresa número uno de mayor calidad, en todo méxico, danos la oportunidad'));
+    }
+}
---
+++ AluminiosYObras/database/seeds/login.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class login extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+              DB::table('login')-> insert(array('correoElectronico'=>'admin@aluminiosyobras.com.mx',
+            'contrasena'=>md5('Admi12//1')));
+    }
+}

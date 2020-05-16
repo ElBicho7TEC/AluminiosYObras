3faf3a140991eed01b5fcd27776604c450037f25
Javier-Ramirez <javi.guti.rama@gmail.com>
2020-05-16 10:58:39 -0500

    Responsividad al 100

--- AluminiosYObras/resources/views/single.blade.php
+++ AluminiosYObras/resources/views/single.blade.php
@@ -76,8 +76,8 @@
 			{
 				$('#etalage').etalage(
 				{
-					thumb_image_width: 400,
-					thumb_image_height: 400,
+					thumb_image_width: 300,
+					thumb_image_height: 300,
 										
 					show_hint: true,
 					click_callback: function(image_anchor, instance_id)
@@ -108,150 +108,203 @@
 		</script>					
 	</head>
 	<body>
-	  <header class="section page-header header-absolute">
-		<div style="transition: 0.35s all cubic-bezier(0.65, 0.05, 0.36, 1);">
-          <nav >
-			  <div class="social_single" align="right" style="margin-right: 5%">
-	            <!-- RD Navbar Nav-->
-	            <ul  style="background-color: white !important; padding-top: 20px; padding-bottom: 20px;">
-	              <font >
-	              <li style="margin-left: 70px;" class="rd-nav-item active"><a class="rd-nav-link" href="/AluminiosYObras/AluminiosYObras/public/#home">Inicio</a></li>
-	              <li style="margin-left: 70px;" class="rd-nav-item"><a class="rd-nav-link" href="/AluminiosYObras/AluminiosYObras/public/#portfolio">Portafolio</a></li>
-	              <li style="margin-left: 70px;" class="rd-nav-item"><a class="rd-nav-link" href="/AluminiosYObras/AluminiosYObras/public/#services">Servicios</a></li>
-	              <li style="margin-left: 70px;" class="rd-nav-item"><a class="rd-nav-link" href="/AluminiosYObras/AluminiosYObras/public/#team">Equipo</a></li>
-	              <li style="margin-left: 70px;" class="rd-nav-item"><a class="rd-nav-link" href="/AluminiosYObras/AluminiosYObras/public/#contacts">Contactos</a></li>
-	              </font>
-	            </ul>
+		<div class="header-top">
+	 		<div class="wrap"> 
+				<div class="logo">
+					<a href="/AluminiosYObras/AluminiosYObras/public/"><img src="../storage/images/negro.png" style="width:140px !important; height:90px !important;" /></a>
+	    		</div>
+				<div class="clear"></div>
+ 				</div>
+   			</div>
+   <div class="header-bottom">
+   	<div class="wrap">
+   		<!-- start header menu -->
+		<ul class="megamenu skyblue">
+		    <li class="rd-nav-item active"><a class="color5" href="/AluminiosYObras/AluminiosYObras/public/#home">Inicio</a></li>
+			<li class="rd-nav-item"><a class="color5" href="/AluminiosYObras/AluminiosYObras/public/#portfolio">Portafolio</a></li>
+  			<li class="rd-nav-item"><a class="color5" href="/AluminiosYObras/AluminiosYObras/public/#services">Servicios</a></li>				
+			<li class="rd-nav-item"><a class="color5" href="/AluminiosYObras/AluminiosYObras/public/#team">Equipo</a></li>
+			<li class="rd-nav-item"><a class="color5" href="/AluminiosYObras/AluminiosYObras/public/#contacts">Contactos</a>
+		   </ul>
+		   <div class="clear"></div>
+     	</div>
+    </div>
+       	<div class="single">
+         	<div class="wrap" ALIGN="justify">
+				<div class="cont span_2_of_3">
+			  		<div class="labout span_1_of_a1">
+						<!-- start product_slider -->
+				     	<ul id="etalage">
+							<li>
+								<a href="optionallink.html">
+									<img class="etalage_thumb_image" src="../storage/app/public{{$datosGaleria[0]->rutafotoprincipal}}" />
+									<img class="etalage_source_image" src="../storage/app/public{{$datosGaleria[0]->rutafotoprincipal}}" />
+								</a>
+							</li>
+							@foreach ($datosFotoGaleria as $foto)
+							<li>
+								<img class="etalage_thumb_image" src="../storage/app/public{{$foto->fotos}}" />
+								<img class="etalage_source_image" src="../storage/app/public{{$foto->fotos}}" />
+							</li>
+							@endforeach
+						</ul>		
+						<!-- end product_slider -->
+					</div>
+					<div class="cont1 span_2_of_a1">
+						<h3 class="m_3" >{{$datosGaleria[0]->nombreproyecto}}</h3>
+	    				<p class="m_desc" >{{$datosGaleria[0]->descripcionlarga}}</p>
+	                	<div class="social_single">	
+						   	<ul>	
+							  	<li class="fb"><a href="#"><span> </span></a></li>
+							  	<li class="tw"><a href="#"><span> </span></a></li>
+							  	<li class="g_plus"><a href="#"><span> </span></a></li>
+							  	<li class="rss"><a href="#"><span> </span></a></li>		
+						   	</ul>
+			    		</div>
+			    		<div align="center">
+			    			<br>
+			    			<br>
+			    		</div>
+			    		<div align="center">
+			    			<form action="index" method="get" enctype="multipart/form-data">
+				            {{ csrf_field() }}
+				              	<button id="button" class="button button-default">Volver</button> 
+				              	<br>
+			    			<br>
+				            </form>
+			    		</div>
+					</div>
+					<div class="clear"></div>
+					    <script type="text/javascript">
+							$(window).load(function() 
+							{
+								$("#flexiselDemo1").flexisel();
+								$("#flexiselDemo2").flexisel(
+								{
+									enableResponsiveBreakpoints: true,
+							    	responsiveBreakpoints: 
+							    	{ 
+							    		portrait: 
+							    		{ 
+							    			changePoint:480,
+							    			visibleItems: 1
+							    		}, 
+							    		landscape: 
+							    		{ 
+							    			changePoint:640,
+							    			visibleItems: 2
+							    		},
+							    		tablet: 
+							    		{ 
+							    			changePoint:768,
+							    			visibleItems: 3
+							    		}
+							    	}
+							    });
+							
+								$("#flexiselDemo3").flexisel(
+								{
+									visibleItems: 5,
+									animationSpeed: 1000,
+									autoPlay: true,
+									autoPlaySpeed: 3000,    		
+									pauseOnHover: true,
+									enableResponsiveBreakpoints: true,
+							    	responsiveBreakpoints: 
+							    	{ 
+							    		portrait: 
+							    		{ 
+							    			changePoint:480,
+							    			visibleItems: 1
+							    		}, 
+							    		landscape: 
+							    		{ 
+							    			changePoint:640,
+							    			visibleItems: 2
+							    		},
+							    		tablet: 
+							    		{ 
+							    			changePoint:768,
+							    			visibleItems: 3
+							    		}
+							    	}
+							    });
+							    
+							});
+						</script>
+						<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+	     			</div>
+	     		<div class="clear"></div>
+		 	</div>
+	    </div>
+	    <script type="text/javascript">
+			$(document).ready(function() 
+			{	
+				var defaults = 
+				{
+			  		containerID: 'toTop', // fading element id
+					containerHoverID: 'toTopHover', // fading element hover id
+					scrollSpeed: 1200,
+					easingType: 'linear' 
+			 	};
+				$().UItoTop({ easingType: 'easeOutQuart' });		
+			});
+		</script>
+	    <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+	    <!-- Page Footer-->
+	    <div class="pre-footer-classic bg-gray-700 context-dark">
+	        <div class="container">
+	          	<div class="row row-30 justify-content-lg-between">
+	            	<div class="col-sm-6 col-lg-3 col-xl-3" >
+		              	<h5>Ubicación</h5>
+		              	<ul class="list list-sm" style="background-color: transparent;">
+			                <li>
+			                  <p>1418 Riverwood Drive,</p>
+			                </li>
+			                <li>
+			                  <p>Suite 3845 Cottonwood,</p>
+			                </li>
+			                <li>
+			                  <p>CA 96022</p>
+			                </li>
+			                <li>
+			                  <p>México</p>
+			                </li>
+		              	</ul>
+		            </div>
+	            <div class="col-sm-6 col-lg-4 col-xl-3">
+	              <h5>Contactos</h5>
+	              <dl class="list-terms-custom">
+	                <dt>Tel.</dt>
+	                <dd><a class="link-default" href="tel:#">1-300-123-1234</a></dd>
+	              </dl>
+	              <dl class="list-terms-custom">
+	                <dt>Correo.</dt>
+	                <dd><a class="link-default" href="mailto:#">Info@demolink.org</a></dd>
+	              </dl>
+	              <ul class="list-inline list-inline-sm">
+	                <li><a class="icon icon-sm icon-gray-filled icon-circle mdi mdi-facebook" href="#"></a></li>
+	                <li><a class="icon icon-sm icon-gray-filled icon-circle mdi mdi-instagram" href="#"></a></li>
+	                <li><a class="icon icon-sm icon-gray-filled icon-circle mdi mdi-whatsapp" href="#"></a></li>
+	                <li><a class="icon icon-sm icon-gray-filled icon-circle mdi mdi-twitter" href="#"></a></li>
+	              </ul>
+	            </div>
+	            <div class="col-lg-4">
+	              <h5>Boletin informativo</h5>
+	              <form class="rd-form rd-mailform" data-form-output="form-output-global" data-form-type="contact" method="post" action="templete/bat/rd-mailform.php">
+	                <div class="form-wrap form-wrap-icon">
+	                  <div class="form-icon mdi mdi-email-outline"></div>
+	                  <input class="form-input" id="footer-email" type="email" name="email" data-constraints="@Email @Required">
+	                  <label class="form-label" for="footer-email">Correo electrónico</label>
+	                </div>
+	                <div class="button-wrap">
+	                  <button class="button button-default button-invariable" type="submit">Suscribir</button>
+	                </div>
+	              </form>
+	            </div>
 	          </div>
-          </nav>
-        </div>
-        <hr size="2px" color="#E9E9E9" />
-      </header>
-
-		 <section class="section section-lg bg-default section-lined">
-        <div class="container container-lined">
-          <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4"></div>
-            <div class="col-md-4"></div>
-          </div>
-        </div>
-        <div >
-          <div >
-	      	<!-- ------------------------------------------------------------------------------------------ -->
-	      	<!-- ------------------------------------------------------------------------------------------ -->
-	      	<!-- ------------------------------------------------------------------------------------------ -->
-	      	<!-- ------------------------------------------------------------------------------------------ -->
-	      	<!-- ------------------------------------------------------------------------------------------ -->
-		  
-		   		<div class="wrap" ALIGN="justify">
-		   			<!-- start header menu -->	
-					<div class="cont span_2_of_3">
-					  	<div class="labout span_1_of_a1">
-							<!-- start product_slider -->
-						    <ul id="etalage">
-								<li>
-									<a href="optionallink.html">
-										<img class="etalage_thumb_image" src="../storage/app/public{{$datosGaleria[0]->rutafotoprincipal}}" />
-										<img class="etalage_source_image" src="../storage/app/public{{$datosGaleria[0]->rutafotoprincipal}}" />
-									</a>
-								</li>
-								@foreach ($datosFotoGaleria as $foto)
-								<li>
-									<img class="etalage_thumb_image" src="../storage/app/public{{$foto->fotos}}" />
-									<img class="etalage_source_image" src="../storage/app/public{{$foto->fotos}}" />
-								</li>
-								@endforeach
-							</ul>	
-							<!-- end product_slider -->
-						</div>
-						
-						<div class="cont1 span_2_of_a1" >
-							<h3 class="m_3" style="margin-left: 100px">{{$datosGaleria[0]->nombreproyecto}}</h3>
-						<div class="btn_form">	  
-					</div>
-		    		
-		    		<p class="m_desc" style="margin-left: 100px;">{{$datosGaleria[0]->descripcionlarga}}</p>
-		    			
-		            <div class="social_single" style="margin-left: 100px">	
-						<ul>	
-							<li class="fb"><a href="#"><span> </span></a></li>
-							<li class="tw"><a href="#"><span> </span></a></li>
-							<li class="g_plus"><a href="#"><span> </span></a></li>
-							<li class="rss"><a href="#"><span> </span></a></li>		
-						</ul>
-					</div>
-					<div class="btn_form" align="center" style="margin-left: 100px">
-					   <form>
-						  <div class="button-wrap-1 text-center"><a class="button button-default" href="/AluminiosYObras/AluminiosYObras/public/">Volver</a></div>
-					  </form>
-					</div>
-				</div>
-			</div>
-			<!-- ------------------------------------------------------------------------------------------ -->
-	      	<!-- ------------------------------------------------------------------------------------------ -->
-	      	<!-- ------------------------------------------------------------------------------------------ -->
-	      	<!-- ------------------------------------------------------------------------------------------ -->
-	      	<!-- ------------------------------------------------------------------------------------------ -->
-      	  </div>     
-        </div>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-      </section>
-
-      <!-- Page Footer-->
-      <div class="pre-footer-classic bg-gray-700 context-dark">
-        <div class="container">
-          <div class="row row-30 justify-content-lg-between">
-            <div class="col-sm-6 col-lg-3 col-xl-3" >
-              <h5>Ubicación</h5>
-              <ul class="list list-sm" style="background-color: transparent;">
-                <li>
-                  <p>1418 Riverwood Drive,</p>
-                </li>
-                <li>
-                  <p>Suite 3845 Cottonwood,</p>
-                </li>
-                <li>
-                  <p>CA 96022</p>
-                </li>
-                <li>
-                  <p>México</p>
-                </li>
-              </ul>
-            </div>
-            <div class="col-sm-6 col-lg-4 col-xl-3">
-              <h5>Contactos</h5>
-              <dl class="list-terms-custom">
-                <dt>Tel.</dt>
-                <dd><a class="link-default" href="tel:#">1-300-123-1234</a></dd>
-              </dl>
-              <dl class="list-terms-custom">
-                <dt>Correo.</dt>
-                <dd><a class="link-default" href="mailto:#">Info@demolink.org</a></dd>
-              </dl>
-              <ul class="list-inline list-inline-sm">
-                <li><a class="icon icon-sm icon-gray-filled icon-circle mdi mdi-facebook" href="#"></a></li>
-                <li><a class="icon icon-sm icon-gray-filled icon-circle mdi mdi-instagram" href="#"></a></li>
-                <li><a class="icon icon-sm icon-gray-filled icon-circle mdi mdi-whatsapp" href="#"></a></li>
-                <li><a class="icon icon-sm icon-gray-filled icon-circle mdi mdi-twitter" href="#"></a></li>
-              </ul>
-            </div>
-            <div class="col-lg-4">
-              <h5>Boletin informativo</h5>
-              <form class="rd-form rd-mailform" data-form-output="form-output-global" data-form-type="contact" method="post" action="templete/bat/rd-mailform.php">
-                <div class="form-wrap form-wrap-icon">
-                  <div class="form-icon mdi mdi-email-outline"></div>
-                  <input class="form-input" id="footer-email" type="email" name="email" data-constraints="@Email @Required">
-                  <label class="form-label" for="footer-email">Correo electrónico</label>
-                </div>
-                <div class="button-wrap">
-                  <button class="button button-default button-invariable" type="submit">Suscribir</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+	        </div>
+	    </div>
       <footer class="section footer-classic context-dark text-center">
         <div class="container">
           <div class="row row-15">
